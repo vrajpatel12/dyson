a91306cd73495e56b5a54567636c2e3785a426dd
Vraj Patel <75464029+vrajpatel2451@users.noreply.github.com>
2022-03-21 20:26:27 +0530

    Update Offer.jsx

--- components/Offer.jsx
+++ components/Offer.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 const Offer = () => {
   return (
     <div className='offer'>
-        <h2>Special edition Dyson Supersonic™ hair dryer (Prussian blue/rich copper) FREE</h2>
+        <h2>Special edition Dyson Supersonic™ hair dryer (Prussian blue/rich copper) Free</h2>
         
         <a href='https://bluclixmedia.o18.click/c?o=17096764&m=5315&a=325185&sub_aff_id={pavan}' target={"_blank"} rel="noreferrer"><button>Claim It Now</button></a>
     </div>
