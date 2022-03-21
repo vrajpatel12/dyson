5420c898453a757b5ca3e31ff1f2736837bffcf9
Krupal Chaudhary <krupalchaudhary2000@gmail.com>
2022-03-22 01:01:24 +0530

    1pm commited

---
+++ components/Label.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+const Label = () => {
+  return (
+    <div className="label-circle">
+      <div className="label-text">
+        special <br />
+        offer
+      </div>
+    </div>
+  );
+};
+
+export default Label;
